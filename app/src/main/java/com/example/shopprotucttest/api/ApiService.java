d08f0c7d0d3784274e5749fa781dce47a629b3bc
Neyasbit <kobzevjob@gmail.com>
2019-08-06 01:29:15 +0700

    updated Api

--- app/src/main/java/com/example/shopprotucttest/api/ApiService.java
+++ app/src/main/java/com/example/shopprotucttest/api/ApiService.java
@@ -16,4 +16,7 @@ public interface ApiService {
 
     @GET("shop-item")
     Call<ShopData> getDetailCategories(@Query("shop_item_type") String type, @Query("book_category_id") int category_id);
+
+    @GET("shop-item")
+    Call<ShopData> getRefreshDataList(@Query("shop_item") String par);
 }
